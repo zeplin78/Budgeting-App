7008cf29b2a7fbcaecda1eb4c779a7def7001a69
zeplin78 <muffi3333@gmail.com>
2024-10-23 21:14:33 +0800

    adding base functions and layouts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,54 @@
 
 import { useLoaderData } from "react-router-dom";
 
+// library imports
+import { toast } from "react-toastify";
+
+// components
+import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
+
 //  helper functions
 import { fetchData } from "../helpers"
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName }
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
+}
+
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data)
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
+    return toast.success(`Welcome, ${formData.userName}`)
+  } catch (e) {
+    throw new Error("There was a problem creating your account.")
+  }
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName, budgets } = useLoaderData()
 
   return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : <Intro />}
+    </>
   )
 }
 export default Dashboard
